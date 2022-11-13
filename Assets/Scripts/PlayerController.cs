ed943a0079651dcc44ac88d26b3a40bfd73149dc
umjcle <90406181+umjcle@users.noreply.github.com>
2022-11-13 05:27:25 -0500

    fix player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,19 +17,31 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        if (movement != Vector2.zero)
-        {
-            int count = rb.Cast(
-                movement,
-                movementFilter,
-                castCollisions,
-                moveSpeed * Time.fixedDeltaTime + collisionOffset
-            );
-            if (count == 0) {
-                rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (movement != Vector2.zero) {
+            bool success = TryMove(movement);
+            if (!success) {
+                success = TryMove(new Vector2(movement.x, 0));
+                if (!success) {
+                    success = TryMove(new Vector2(0, movement.y));
+                }
             }
+        }
+    }
+
+    private bool TryMove(Vector2 dir) {
+        int count = rb.Cast(
+            movement,
+            movementFilter,
+            castCollisions,
+            moveSpeed * Time.fixedDeltaTime + collisionOffset
+        );
+        if (count == 0) {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        } else {
+            return false;
         }
     }
 
