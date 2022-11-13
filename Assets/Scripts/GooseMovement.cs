b2934cf06e30eeda97e5deaed714aafe4d909e5f
NEW777 <nwierzbow@gmail.com>
2022-11-13 06:36:51 -0500

    Added goose movement

---
+++ Assets/Scripts/GooseMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooseMovement : MonoBehaviour
+{
+    public float moveSpeed = 8f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+    Vector2 movement;
+    Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.position) > 1f)
+        {//move if distance from target is greater than 1
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+    }
+}
